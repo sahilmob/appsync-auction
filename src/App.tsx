6b03efbcab322bb91c427ec4b3f41f4e6003980e
Sahil Mobaidin <sahil@ibtmena.com>
2019-04-08 10:32:47 +0300

    finish auth

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,17 @@ import React, { Component } from "react";
 
 import { Auctions } from "./Auctions";
 import { CreateAuctionForm } from "./CreateAuctionForm";
+import { withAuthenticator } from "aws-amplify-react";
 
 class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<CreateAuctionForm />
-				<Auctions />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="App">
+        <CreateAuctionForm />
+        <Auctions />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withAuthenticator(App);
--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,34 @@
 import * as serviceWorker from "./serviceWorker";
 
 import AWSAppSyncClient, { AUTH_TYPE } from "aws-appsync";
+import Amplify, { Auth } from "aws-amplify";
 
 import { ApolloProvider } from "react-apollo";
 import App from "./App";
 import AppSyncConfig from "./aws-exports";
+import { AuthLink } from "aws-appsync/lib/link";
 import React from "react";
 import ReactDOM from "react-dom";
 
+Amplify.configure(AppSyncConfig);
+
 const client = new AWSAppSyncClient({
-	disableOffline: true,
-	url: AppSyncConfig.aws_appsync_graphqlEndpoint,
-	region: AppSyncConfig.aws_appsync_region,
-	auth: {
-		type: AppSyncConfig.aws_appsync_authenticationType as AUTH_TYPE,
-		apiKey: AppSyncConfig.aws_appsync_apiKey
-		// jwtToken: async () => token, // Required when you use Cognito UserPools OR OpenID Connect. token object is obtained previously
-	}
+  disableOffline: true,
+  url: AppSyncConfig.aws_appsync_graphqlEndpoint,
+  region: AppSyncConfig.aws_appsync_region,
+  auth: {
+    type: AUTH_TYPE.AMAZON_COGNITO_USER_POOLS,
+    jwtToken: async () =>
+      (await Auth.currentSession()).getAccessToken().getJwtToken()
+    // jwtToken: async () => token, // Required when you use Cognito UserPools OR OpenID Connect. token object is obtained previously
+  }
 });
 
 ReactDOM.render(
-	<ApolloProvider client={client}>
-		<App />
-	</ApolloProvider>,
-	document.getElementById("root")
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
