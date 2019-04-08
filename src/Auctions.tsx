ff3d2b8e61c4b69e14cf949e8130398863099d2b
Sahil Mobaidin <sahil@ibtmena.com>
2019-04-08 09:41:54 +0300

    finish subscriptions

--- src/Auctions.tsx
+++ src/Auctions.tsx
@@ -1,42 +1,52 @@
 import { ListAuctionsQuery, ListAuctionsQueryVariables } from "./API";
 
 import { AuctionCard } from "./AuctionCard";
+import { OnMount } from "./components/OnMount";
 import { Query } from "react-apollo";
 import React from "react";
+import { buildSubscription } from "aws-appsync";
 import gql from "graphql-tag";
 import { listAuctions } from "./graphql/queries";
+import { onCreateAuction } from "./graphql/subscriptions";
 
 export function Auctions() {
-	return (
-		<Query<ListAuctionsQuery, ListAuctionsQueryVariables>
-			query={gql(listAuctions)}
-			variables={{ limit: 100 }}
-		>
-			{({ data, loading }) => {
-				if (
-					loading ||
-					!data ||
-					!data.listAuctions ||
-					!data.listAuctions.items
-				) {
-					return null;
-				} else {
-					console.log(data.listAuctions.items);
-					return (
-						<div
-							style={{
-								display: "grid",
-								gridTemplateColumns: "repeat(4, 1fr)",
-								gridGap: 10
-							}}
-						>
-							{data!.listAuctions.items.map(a => (
-								<AuctionCard key={a!.id} name={a!.name} price={a!.price} />
-							))}
-						</div>
-					);
-				}
-			}}
-		</Query>
-	);
+  return (
+    <Query<ListAuctionsQuery, ListAuctionsQueryVariables>
+      query={gql(listAuctions)}
+      variables={{ limit: 100 }}
+    >
+      {({ data, loading, subscribeToMore }) => {
+        if (
+          loading ||
+          !data ||
+          !data.listAuctions ||
+          !data.listAuctions.items
+        ) {
+          return null;
+        } else {
+          console.log(data.listAuctions.items);
+          return (
+            <div
+              style={{
+                display: "grid",
+                gridTemplateColumns: "repeat(4, 1fr)",
+                gridGap: 10
+              }}
+            >
+              <OnMount
+                onEffect={() => {
+                  return subscribeToMore(
+                    buildSubscription(gql(onCreateAuction), gql(listAuctions))
+                  );
+                }}
+              />
+              {data!.listAuctions.items.map(a => (
+                <AuctionCard key={a!.id} name={a!.name} price={a!.price} />
+              ))}
+            </div>
+          );
+        }
+      }}
+    </Query>
+  );
 }
--- src/CreateAuctionForm.tsx
+++ src/CreateAuctionForm.tsx
@@ -1,7 +1,7 @@
 import {
-	CreateAuctionMutation,
-	CreateAuctionMutationVariables,
-	ListAuctionsQuery
+  CreateAuctionMutation,
+  CreateAuctionMutationVariables,
+  ListAuctionsQuery
 } from "./API";
 
 import Button from "@material-ui/core/Button";
@@ -15,90 +15,90 @@ import { listAuctions } from "./graphql/queries";
 import { produce } from "immer";
 
 interface FormValues {
-	name: string;
-	price: number;
+  name: string;
+  price: number;
 }
 
 export function CreateAuctionForm() {
-	return (
-		<Mutation<CreateAuctionMutation, CreateAuctionMutationVariables>
-			mutation={gql(createAuction)}
-		>
-			{createAuction => (
-				<Formik<FormValues>
-					initialValues={{
-						name: "",
-						price: 0
-					}}
-					onSubmit={async ({ name, price }, { resetForm }) => {
-						const response = await createAuction({
-							variables: {
-								input: {
-									name,
-									price
-								}
-							},
-							optimisticResponse: {
-								createAuction: {
-									__typename: "Auction",
-									id: "-1",
-									name,
-									price
-								}
-							},
-							// this is what returned from createAuction mutation
-							update: (store, { data }) => {
-								if (!data || !data.createAuction) {
-									return;
-								}
-								const auction = store.readQuery<ListAuctionsQuery>({
-									query: gql(listAuctions),
-									variables: { limit: 100 }
-								});
+  return (
+    <Mutation<CreateAuctionMutation, CreateAuctionMutationVariables>
+      mutation={gql(createAuction)}
+    >
+      {createAuction => (
+        <Formik<FormValues>
+          initialValues={{
+            name: "",
+            price: 0
+          }}
+          onSubmit={async ({ name, price }, { resetForm }) => {
+            const response = await createAuction({
+              variables: {
+                input: {
+                  name,
+                  price
+                }
+              },
+              optimisticResponse: {
+                createAuction: {
+                  __typename: "Auction",
+                  id: "-1",
+                  name,
+                  price
+                }
+              }
+              // this is what returned from createAuction mutation
+              // update: (store, { data }) => {
+              // 	if (!data || !data.createAuction) {
+              // 		return;
+              // 	}
+              // 	const auction = store.readQuery<ListAuctionsQuery>({
+              // 		query: gql(listAuctions),
+              // 		variables: { limit: 100 }
+              // 	});
 
-								store.writeQuery({
-									query: gql(listAuctions),
-									variables: {
-										limit: 100
-									},
-									data: produce(auction, ds => {
-										ds!.listAuctions!.items!.unshift(data.createAuction);
-									})
-								});
-							}
-							// refetchQueries: [
-							// 	{ query: gql(listAuctions), variables: { limit: 100 } }
-							// ] this is one way of getting the new data after submitting an auction
-						});
-						resetForm();
-						console.log(response);
-					}}
-				>
-					{({ values, handleChange, handleSubmit }) => (
-						<form onSubmit={handleSubmit}>
-							<TextField
-								name="name"
-								label="Name"
-								value={values.name}
-								onChange={handleChange}
-								margin="normal"
-							/>
-							<br />
-							<TextField
-								name="price"
-								label="Price"
-								value={values.price}
-								onChange={handleChange}
-								margin="normal"
-							/>
-							<br />
-							<Button variant="contained" type="submit">
-								Submit
-							</Button>
-						</form>
-					)}
-				</Formik>
-			)}
-		</Mutation>
-	);
+              // 	store.writeQuery({
+              // 		query: gql(listAuctions),
+              // 		variables: {
+              // 			limit: 100
+              // 		},
+              // 		data: produce(auction, ds => {
+              // 			ds!.listAuctions!.items!.unshift(data.createAuction);
+              // 		})
+              // 	});
+              // }
+              // refetchQueries: [
+              // 	{ query: gql(listAuctions), variables: { limit: 100 } }
+              // ] this is one way of getting the new data after submitting an auction
+            });
+            resetForm();
+            console.log(response);
+          }}
+        >
+          {({ values, handleChange, handleSubmit }) => (
+            <form onSubmit={handleSubmit}>
+              <TextField
+                name="name"
+                label="Name"
+                value={values.name}
+                onChange={handleChange}
+                margin="normal"
+              />
+              <br />
+              <TextField
+                name="price"
+                label="Price"
+                value={values.price}
+                onChange={handleChange}
+                margin="normal"
+              />
+              <br />
+              <Button variant="contained" type="submit">
+                Submit
+              </Button>
+            </form>
+          )}
+        </Formik>
+      )}
+    </Mutation>
+  );
 }
